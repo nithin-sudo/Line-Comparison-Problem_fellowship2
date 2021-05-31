e5a34d266fc5580fcf30da9d92f61351467c5365
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-05-31 20:49:38 +0530

    calculating distance between two lines

--- comparision.java
+++ comparision.java
@@ -1,8 +1,20 @@
 package comparision;
-
+import java.util.Scanner;
 public class comparision {
 	public static void main(String[] args){
-		System.out.println("Welcome to Line Comparison Computation");
-	}
+				int x1,x2,y1,y2;
+				double distance;
+				Scanner sc = new Scanner(System.in);
+				System.out.println("enter x1 point:");
+				x1=sc.nextInt();
+				System.out.println("enter x2 point:");
+				x2=sc.nextInt();
+				System.out.println("enter y1 point:");
+				y1=sc.nextInt();
+				System.out.println("enter y2 point:");
+				y2=sc.nextInt();
+				distance=Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
+				System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")"+"is"+" " + distance);
+			}
 
-}
+	}
